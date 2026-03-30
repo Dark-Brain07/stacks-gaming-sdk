83b2aca438a573179ec52cece1700f19c06305be
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:08:11 +0600

    refactor: add math utils set 39

---
+++ src/utils/math/fn39.ts
@@ -0,0 +1,3 @@
+export const lerp39=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp39=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep39=(e0:number,e1:number,x:number):number=>{const t=clamp39((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
