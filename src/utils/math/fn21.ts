79aec9b6994f0574eaf5b00406881a8c7abd7088
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:08:03 +0600

    refactor: add math utils set 21

---
+++ src/utils/math/fn21.ts
@@ -0,0 +1,3 @@
+export const lerp21=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp21=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep21=(e0:number,e1:number,x:number):number=>{const t=clamp21((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
