1057bd26009a70514ec2fba274f87f45955f8ff7
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:08:05 +0600

    refactor: add math utils set 24

---
+++ src/utils/math/fn24.ts
@@ -0,0 +1,3 @@
+export const lerp24=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp24=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep24=(e0:number,e1:number,x:number):number=>{const t=clamp24((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
