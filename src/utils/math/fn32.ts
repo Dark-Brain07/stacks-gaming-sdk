8877ffe500c54a48a0c87b9e10b2ee52529f40a1
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:08:08 +0600

    refactor: add math utils set 32

---
+++ src/utils/math/fn32.ts
@@ -0,0 +1,3 @@
+export const lerp32=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp32=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep32=(e0:number,e1:number,x:number):number=>{const t=clamp32((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
