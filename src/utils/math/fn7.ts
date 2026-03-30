b57a84983353519ff7bb7861e41ed7561b960b8c
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:07:58 +0600

    refactor: add math utils set 7

---
+++ src/utils/math/fn7.ts
@@ -0,0 +1,3 @@
+export const lerp7=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp7=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep7=(e0:number,e1:number,x:number):number=>{const t=clamp7((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
