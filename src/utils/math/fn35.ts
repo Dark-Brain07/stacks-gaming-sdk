1a46eddd78ef2c62c3165e4a72c71c73d22eb0a6
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:08:09 +0600

    refactor: add math utils set 35

---
+++ src/utils/math/fn35.ts
@@ -0,0 +1,3 @@
+export const lerp35=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp35=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep35=(e0:number,e1:number,x:number):number=>{const t=clamp35((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
