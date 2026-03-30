9702e80f45800429039c8a000eba62acb1ea1c52
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 21:07:57 +0600

    refactor: add math utils set 5

---
+++ src/utils/math/fn5.ts
@@ -0,0 +1,3 @@
+export const lerp5=(a:number,b:number,t:number):number=>a+(b-a)*t;
+export const clamp5=(v:number,min:number,max:number):number=>Math.max(min,Math.min(max,v));
+export const smoothstep5=(e0:number,e1:number,x:number):number=>{const t=clamp5((x-e0)/(e1-e0),0,1);return t*t*(3-2*t);};
