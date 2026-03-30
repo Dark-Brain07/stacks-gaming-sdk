86b9d7220616f095ba5775b2e22a01cd2d5174d7
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-30 07:54:08 +0600

    feat: create main SDK entry point

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './wallet';
+export * from './contracts';
+export * from './analytics';
+export * from './chainhooks';
+export * from './leaderboard';
+export const SDK_VERSION = '1.0.0';
