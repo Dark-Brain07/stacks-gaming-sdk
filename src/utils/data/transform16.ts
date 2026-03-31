93a5abdd7f1c2754e5d2dd8e2202bc32efd344fd
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-31 08:56:02 +0600

    refactor: add data transform utils 16

---
+++ src/utils/data/transform16.ts
@@ -0,0 +1,3 @@
+export function pipe16<T>(...fns:((v:T)=>T)[]):(v:T)=>T{return(v)=>fns.reduce((acc,fn)=>fn(acc),v);}
+export function memoize16<T>(fn:(...args:any[])=>T):(...args:any[])=>T{const cache=new Map();return(...args)=>{const k=JSON.stringify(args);if(!cache.has(k))cache.set(k,fn(...args));return cache.get(k)!;};}
+export function debounce16(fn:Function,ms:number){let t:any;return(...a:any[])=>{clearTimeout(t);t=setTimeout(()=>fn(...a),ms);}}
